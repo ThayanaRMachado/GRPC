18c15e4b996a3af80e2142a4c57584afc4746ab9
ThayanaRMachado <thayanarm@hotmail.com>
2021-05-12 16:57:48 -0300

    Introdução a protobuf

---
+++ src/main/kotlin/br/com/zup/edu/TestandoProtobuf.kt
@@ -0,0 +1,31 @@
+package br.com.zup.edu
+
+import java.io.FileInputStream
+import java.io.FileOutputStream
+
+fun main(args: Array<String>) {
+
+    val request = FuncionarioRequest.newBuilder()
+        .setNome("Yuri Matheus")
+        .setCpf("000.000.000-00")
+        .setIdade(22)
+        .setSalario(2020.0)
+        .setAtivo(true)
+        .setCargo(Cargo.DEV)
+        .addEnderecos(Endereco.newBuilder()
+            .setLogradouro("Rua dos Tabajaras, 123")
+            .setCep("00000-00")
+            .setComplemento("Atrás do shopping")
+            .build()
+        )
+        .build()
+    println(request)
+    request.writeTo(FileOutputStream("funcionario-request.bin"))
+
+    val r2 = FuncionarioRequest.newBuilder()
+        .mergeFrom(FileInputStream("funcionario-request.bin"))
+
+    r2.setCargo(Cargo.GERENTE).build()
+
+    println(r2)
+}
